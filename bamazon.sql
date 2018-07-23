2dd54e8ac9c05c2b860eb20af5bbfab6fae97788
Lindsay <lindnerdesign@gmail.com>
2018-07-23 13:23:53 -0500

    update 7_23

--- bamazon.sql
+++ bamazon.sql
@@ -8,47 +8,47 @@ CREATE TABLE products(
   product_name VARCHAR(50) NOT NULL,
   department_name VARCHAR(50) NOT NULL,
   price INTEGER DEFAULT 0,
-  stock_quanity INTEGER DEFAULT 0,
+  stock_quantity INTEGER DEFAULT 0,
   PRIMARY KEY (id)
 );
 
 
 -- one
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("workout leggings", "womens", 50, 5);
 
 -- two
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("gym shoes", "fitness", 125, 5);
 
 -- three
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("blender", "kitchen", 80, 3);
 
 -- four
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("rice cooker", "kitchen", 35, 2);
 
 -- five
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("dog collar", "pets", 15, 5);
 
 -- six
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("dog frisbee", "pets", 12, 5);
 
 -- seven
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("fishing pole", "outdoor", 150, 2);
 
 -- eight
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("grill", "outdoor", 1500, 1);
 
 -- nine
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("tent", "outdoor", 250, 2);
 
 -- ten
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("sunglasses", "womens", 75, 2);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,13 +12,87 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "Summer2015",
+  password: "",
   database: "bamazon_db"
 });
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  // start();
+  // console.log("connected as id " + connection.threadId);
+  // itemsForSale();
 });
+
+// function itemsForSale() {
+//   connection.query("SELECT * FROM products", function(err, res) {
+//     console.log('ITEMS FOR SALE: \n'
+//     + 'ID  |  Product Name  | Department  |  Quanity');
+//     for (var i = 0; i < res.length; i++) {
+//       console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].stock_quanity);
+//     }
+//     console.log("-----------------------------------");
+//   });
+// }
+
+function itemsForSale() {
+  // query the database for all items being auctioned
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i]);
+            }
+            return choiceArray;
+          },
+          message: "Current Items Available on Bamazon!"
+        },
+        {
+          name: "IDandQuantity",
+          type: "input",
+          message: "If you'd like to make a purchase, please specify item [ID] and [Quantity] you need."
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].id && results[i].quantity === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
+
+        // determine if product is still in stock
+        if (chosenItem.quanity < parseInt(0) {
+          // bid was high enough, so update db, let the user know, and start over
+          connection.query(
+            "UPDATE auctions SET ? WHERE ?",
+            [
+              {
+                id: answer.id
+              },
+              {
+                quantity: chosenItem.quanity
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Order has been placed successfully!");
+              // start();
+            }
+          );
+        }
+        else {
+          // Quantity isnt high enough, start over
+          console.log("Insufficent Quantity");
+          // start();
+        }
+      });
+  });
+}
