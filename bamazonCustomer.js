6c10bfae2885e0b38eda4741dd7597f55fda0ca6
Lindsay <lindnerdesign@gmail.com>
2018-07-26 23:16:16 -0500

    tweeks

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,8 @@ var chalk = require("chalk");
 var log = console.log;
 
 var red = chalk.rgb(232, 74, 95);
-var blue = chalk.bold.rgb(41, 63, 92);
+var orange = chalk.rgb(241, 105, 72);
+var blue = chalk.rgb(50, 149, 152);
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
@@ -28,7 +29,7 @@ connection.connect(function(err) {
 });
 
 itemList = () => {
-  log(red('. ..-->>|| WELCOME TO BAMAZON ||<<--.. .'));
+  log(blue('. ..-->>|| WELCOME TO BAMAZON ||<<--.. .'));
   inquirer
     .prompt({
       name: "action",
@@ -39,7 +40,7 @@ itemList = () => {
       if (answer.action === true){
         var query = "SELECT * FROM products";
           connection.query(query, function(err, res) {
-            log(red('\n' + 'BAMAZON PRODUCT LIST \n' + '---------------------------------------------------------------------'));
+            log(orange('\n' + 'BAMAZON PRODUCT LIST \n' + '---------------------------------------------------------------------------------------------'));
             for (var i = 0; i < res.length; i++) {
               log(("ID:" + res[i].id).padEnd(9) + "Product: " + (res[i].product_name).padEnd(20) + "Department: " + (res[i].department_name).padEnd(15) + "Price: " + (res[i].price).toString().padEnd(10) + "Quantity: " + res[i].stock_quantity);
               }
@@ -87,23 +88,23 @@ pickItems = () => {
   			selected = res[0];
 
         if (itemQuantity > selected.stock_quantity && selected.stock_quantity > 1) {
-          log(`Sorry, we only have ${selected.stock_quantity}  " "  ${selected.product_name} s available.`);
+          log(red(`Sorry, we only have ${selected.stock_quantity} ${selected.product_name}s available.`));
           pickItems();
 
         } else if (itemQuantity > selected.stock_quantity && selected.stock_quantity === 1) {
-          log(`Sorry, we only have 1 ${selected.product_name} available.`);
+          log(red(`Sorry, we only have 1 ${selected.product_name} available.`));
           pickItems();
 
         } else if (itemQuantity > selected.stock_quantity && selected.stock_quantity < 1) {
-          log(`Sorry, ${selected.product_name} is out of stock.`);
+          log(red(`Sorry, ${selected.product_name} is out of stock.`));
           pickItems();
 
         } else if (+itemQuantity === 1) {
-          log("You are purchasing 1 " + selected.product_name + ".");
+          log(blue(`You are purchasing 1 ${selected.product_name}.`));
           buyItem();
 
         } else {
-          log(`You are purchasing ${itemQuantity} " " ${selected.product_name} 's.`);
+          log(blue(`You are purchasing ${itemQuantity} ${selected.product_name}'s.`));
           buyItem();
         }
       });
@@ -122,7 +123,7 @@ buyItem = () => {
 		if (answer.buy) {
 			connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?", [itemQuantity, itemID], function(err, res) {
 				if (err) throw err;
-        log(`Your total: $ ${itemQuantity * selected.price}`);
+        log(blue(`Your total: $${itemQuantity * selected.price}`));
         buyDifferentItem();
 			});
 		} else {
@@ -149,6 +150,6 @@ buyDifferentItem = () => {
 }
 
 exit = () => {
-	log("Thank you, please come again!");
+	log(blue("Thank you, please come again!"));
 	connection.end();
 }
